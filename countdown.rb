e384f7b9a13bc34a848a43667ab8ebbf9cdcf6de
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 17:52:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,11 +5,12 @@ def countdown(x)
   while x > 0
   puts "#{x} SECOND(S)!"
   x -= 1
+  
+  
+def countdown_with_sleep(5)
+  sleep (5)
+end 
 end
   return "HAPPY NEW YEAR!"
 end
 
-
-def countdown_with_sleep(5)
-  sleep (5)
-end 
