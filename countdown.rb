bc8a6159182f37f8e05c924e6fa712eed364c582
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-07 17:40:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 #write your code here
-x = 10
+
 
 def countdown(x)
   while x > 0
@@ -9,8 +9,7 @@ end
   return "HAPPY NEW YEAR!"
 end
 
-time = time.now
 
 def countdown_with_sleep(num_secs)
-  sleep 1.seconds until Time.now > time + 10.seconds
+  sleep
 end 
